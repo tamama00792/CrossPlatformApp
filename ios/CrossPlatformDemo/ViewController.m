21d215b35194faed0d5811cc78b580c1fd8ef5a8
satori <tamama00792@gmail.com>
2024-12-19 08:15:01 +0800

    [feat]添加JS

--- ios/CrossPlatformDemo/ViewController.m
+++ ios/CrossPlatformDemo/ViewController.m
@@ -6,6 +6,7 @@
 //
 
 #import "ViewController.h"
+#import <React/RCTRootView.h>
 
 @interface ViewController ()
 
@@ -16,6 +17,40 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    UIButton *test = [UIButton buttonWithType:UIButtonTypeSystem];
+    test.frame = CGRectMake(0, 0, 100, 30);
+    test.center = self.view.center;
+    [test setTitle:@"High Score" forState:UIControlStateNormal];
+    [test addTarget:self action:@selector(highScoreButtonPressed:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:test];
+    
+}
+
+- (void)highScoreButtonPressed:(id)sender {
+    NSLog(@"High Score Button Pressed");
+    NSURL *jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.bundle?platform=ios"];
+
+    RCTRootView *rootView =
+      [[RCTRootView alloc] initWithBundleURL: jsCodeLocation
+                                  moduleName: @"RNHighScores"
+                           initialProperties:
+                             @{
+                               @"scores" : @[
+                                 @{
+                                   @"name" : @"Alex",
+                                   @"value": @"42"
+                                  },
+                                 @{
+                                   @"name" : @"Joel",
+                                   @"value": @"10"
+                                 }
+                               ]
+                             }
+                               launchOptions: nil];
+    UIViewController *vc = [[UIViewController alloc] init];
+    vc.view = rootView;
+    [self presentViewController:vc animated:YES completion:nil];
 }
 
 
