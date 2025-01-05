9b148f44aac23955fd24bf5219cc14b8d5721c6c
satori <tamama00792@gmail.com>
2025-01-05 20:02:46 +0800

    [build]增加babel配置

---
+++ babel.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  presets: ["@babel/preset-env", "@babel/preset-react","@babel/preset-flow", "@babel/preset-typescript" ],
+  plugins: ['react-native-reanimated/plugin'], 
+};
---
+++ metro.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  resolver: {
+    sourceExts: ['jsx',
+         'js', 
+         'ts', 
+         'tsx',
+         'native',
+         'native.js',
+         'native.json',
+         'json',
+         'native.ts',
+         'native.tsx',
+         'ios.js',
+         'android.js'
+        ], // 添加其他扩展名
+  },
+};
